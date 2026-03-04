3d0555804f50c028f8637b70397ec8338f3c74d2
Neha Kakkar <nehaaakakkarr@gmail.com>
2026-03-04 21:22:28 +0530

    more fixes

--- backend/db_models.py
+++ backend/db_models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text, Date, DateTime, ForeignKey, JSON
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from database import Base
+from .database import Base
 import uuid
 
 
